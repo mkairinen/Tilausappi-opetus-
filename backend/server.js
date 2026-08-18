531e291ebdb5282d9e50c2ab850309e788b995ba
mkairinen <131741382+mkairinen@users.noreply.github.com>
2026-08-18 11:07:25 +0300

    Backend serveri (#2)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Serveri toimii');
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+res.end('Hei maailma!');
+});
+
+server.listen(PORT, () => {
+console.log(`Palvelin käynnissä osoitteessa http://localhost:${PORT}`);
+});
